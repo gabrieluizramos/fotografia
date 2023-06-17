6de53a6d6be313e88881be809b2a7a79947f69f0
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2023-06-17 00:09:25 -0300

    remove jpg e insere webp

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,13 +1,15 @@
 import { watch } from 'fs';
 import { extname } from 'path';
 
-import build, { ALL } from './builders/index.js';
+import build, { ALL, SKIP } from './builders/index.js';
 
 const watcher = () => {
     console.log('Starting watcher');
 
     watch('src', { recursive: true }, async (_event, file) => {
+        if (file.includes(SKIP)) return;
         const extension = extname(file);
+
         await build(extension);
     });
 }
